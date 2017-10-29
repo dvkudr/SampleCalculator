351929f3d7710cb59e574a9de78c7613c527b645
dvkudr <dvkudr@gmail.com>
2017-10-29 08:37:32 +0300

    first commit :)

---
+++ CalculatorV1/Interfaces/ICalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CalculatorV1.Interfaces
+{
+    public interface ICalculator
+    {
+        ICalculator Add(int value);
+        ICalculator Sub(int value);
+        ICalculator SignRevert();
+        ICalculator Set(int value);
+        ICalculator Reset();
+        int Result { get; }
+    }
+}
---
+++ CalculatorV1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CalculatorV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
